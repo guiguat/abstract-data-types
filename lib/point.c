2c5a91b190ce5820f833c3e88c7b8855dd2cff7b
guiguat <guaturadzn@gmail.com>
2020-07-31 20:50:49 -0300

    implement setCityCordsAndName

--- lib/point.c
+++ lib/point.c
@@ -9,7 +9,7 @@ City* createCity(float x, float y, char* name) {
     if(c == NULL){
         printf("insufficient memory");
     }
-    strcpy(c->name, name);
+    strcpy_s(c->name, 50, name);
     c->y = y;
     c->x = x;
     return c;
@@ -21,7 +21,11 @@ void freeCityInMemory(City* c) {
 
 City getCityCordsAndName(City * c, float* x, float* y, char * name) {  }
 
-void setCityCordsAndName(City * c, float x, float y, char name[50]) {  }
+void setCityCordsAndName(City * c, float x, float y, char name[50]) {
+    strcpy_s(c->name,50, name);
+    c->y = y;
+    c->x = x;
+}
 
 float getDistanceBetween(City * c1, City * c2) {  }
 
