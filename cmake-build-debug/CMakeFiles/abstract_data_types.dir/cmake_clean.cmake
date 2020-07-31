6dc90773dbbcd64def8e7ac25cf67608982aa391
guiguat <guaturadzn@gmail.com>
2020-07-31 19:39:45 -0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/abstract_data_types.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/abstract_data_types.dir/main.c.obj"
+  "abstract_data_types.exe"
+  "abstract_data_types.exe.manifest"
+  "abstract_data_types.lib"
+  "abstract_data_types.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/abstract_data_types.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
