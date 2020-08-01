873694a246646a6fdd330956a5a7361f237f6d1d
guiguat <guaturadzn@gmail.com>
2020-07-31 21:17:14 -0300

    create route header

---
+++ lib/route.h
@@ -0,0 +1,11 @@
+//
+// Created by Guilherme Guatura on 31/07/2020.
+//
+
+#include "point.h"
+
+#ifndef ABSTRACT_DATA_TYPES_ROUTE_H
+#define ABSTRACT_DATA_TYPES_ROUTE_H
+float calculateTotalDistance(City** cities);
+void printReport(char* c1Name, char* c2Name, float distance);
+#endif //ABSTRACT_DATA_TYPES_ROUTE_H
