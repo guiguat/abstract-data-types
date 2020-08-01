55a80b30f1dc328b0321fb10843a644c71ed07cc
guiguat <guaturadzn@gmail.com>
2020-07-31 22:32:26 -0300

    implement route.h

--- lib/route.c
+++ lib/route.c
@@ -1,9 +1,18 @@
 #include "route.h"
-#include "point.h"
 #include <stdio.h>
 #include <stdlib.h>
 
-float calculateTotalDistance(City** cities){  }
-void printReport(char* c1Name, char* c2Name, float distance){  }
-
-
+float calculateTotalDistance(City** cities){
+    float totalDistance = 0;
+    for (int i=0; i<5; i++){
+        totalDistance += getDistanceBetween(cities[i], cities[i+1]);
+    }
+    return totalDistance;
+}
+void printReport(City* cities[6]){
+    printf("Origin    Destiny    Distance(km)\n");
+    for (int i=0; i<5; i++){
+        printf("%s    %s    %5.2f\n", cities[i]->name, cities[i+1]->name, getDistanceBetween(cities[i], cities[i+1]));
+    }
+    printf("Total distance: %5.2f", calculateTotalDistance(cities));
+}
