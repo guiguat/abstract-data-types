2c35347caa3c97ac6bfbe2a0e6d41572757b9992
guiguat <guaturadzn@gmail.com>
2020-07-31 22:54:04 -0300

    fix wrong coordinates

--- lib/main.c
+++ lib/main.c
@@ -4,10 +4,10 @@ int main() {
     City* cities[6] = {
         createCity(3, 4, "shenzhen"),
         createCity(6, 8, "taipei"),
-        createCity(9, 16, "chongqing"),
-        createCity(12, 20, "beijing"),
-        createCity(15, 24, "shanghai"),
-        createCity(18, 28, "taiwan"),
+        createCity(9, 12, "chongqing"),
+        createCity(12, 16, "beijing"),
+        createCity(15, 20, "shanghai"),
+        createCity(18, 24, "taiwan"),
     };
     printReport(cities);
     return 0;
