64bf11943703c5dd9cdb7a3a0c191188f4103184
guiguat <guaturadzn@gmail.com>
2020-07-31 22:32:34 -0300

    test code

--- lib/main.c
+++ lib/main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
-
+#include "point.h"
+#include "route.h"
 int main() {
-    printf("Hello, World!\n");
+    City* cities[6] = {
+        createCity(3, 4, "shenzhen"),
+        createCity(6, 8, "taipei"),
+        createCity(9, 16, "chongqing"),
+        createCity(12, 20, "beijing"),
+        createCity(15, 24, "shanghai"),
+        createCity(18, 28, "taiwan"),
+    };
+    printReport(cities);
     return 0;
 }
