237184ca8705946ddcbe4fee7722bec3bc81f5cc
guiguat <guaturadzn@gmail.com>
2020-07-31 21:19:54 -0300

    change param to 6 item array

--- lib/route.h
+++ lib/route.h
@@ -6,6 +6,6 @@
 
 #ifndef ABSTRACT_DATA_TYPES_ROUTE_H
 #define ABSTRACT_DATA_TYPES_ROUTE_H
-float calculateTotalDistance(City** cities);
+float calculateTotalDistance(City cities[6]);
 void printReport(char* c1Name, char* c2Name, float distance);
 #endif //ABSTRACT_DATA_TYPES_ROUTE_H
