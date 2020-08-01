aac0e5deca840427e86fd8ee32aa1cc6d63852a7
guiguat <guaturadzn@gmail.com>
2020-07-31 21:17:31 -0300

    implement route

---
+++ lib/route.c
@@ -0,0 +1,9 @@
+#include "route.h"
+#include "point.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+float calculateTotalDistance(City** cities){  }
+void printReport(char* c1Name, char* c2Name, float distance){  }
+
+
