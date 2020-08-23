1abbddad2a83109fb473c6a7e4d0835dbe30c2dc
guiguat <guaturadzn@gmail.com>
2020-08-23 00:14:50 -0300

    insert cities into city list

--- lib/main.c
+++ lib/main.c
@@ -1,14 +1,15 @@
 #include "point.h"
 #include "route.h"
+#include "list.h"
+
 int main() {
-    City* cities[6] = {
-        createCity(2, 2, "shenzhen"),
-        createCity(10, 4, "taipei"),
-        createCity(19, 8, "chongqing"),
-        createCity(16, 15, "beijing"),
-        createCity(5, 19, "shanghai"),
-        createCity(8, 10, "taiwan"),
-    };
-    printReport(cities);
+    List* listOfCities = initializeList();
+    listOfCities = insertListItem(listOfCities, createCity(8,10, "city6"));
+    listOfCities = insertListItem(listOfCities, createCity(5,19, "city5"));
+    listOfCities = insertListItem(listOfCities, createCity(16,15, "city4"));
+    listOfCities = insertListItem(listOfCities, createCity(19,8, "city3"));
+    listOfCities = insertListItem(listOfCities, createCity(10,4, "city2"));
+    listOfCities = insertListItem(listOfCities, createCity(2,2, "city1"));
+    printReport(listOfCities);
     return 0;
 }
