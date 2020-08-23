0cb8df370c1fcecf24bcb2b23365a934833b63c8
guiguat <guaturadzn@gmail.com>
2020-08-22 22:49:17 -0300

    ide changes

--- lib/main.c
+++ lib/main.c
@@ -2,12 +2,12 @@
 #include "route.h"
 int main() {
     City* cities[6] = {
-        createCity(3, 4, "shenzhen"),
-        createCity(6, 8, "taipei"),
-        createCity(9, 12, "chongqing"),
-        createCity(12, 16, "beijing"),
-        createCity(15, 20, "shanghai"),
-        createCity(18, 24, "taiwan"),
+        createCity(2, 2, "shenzhen"),
+        createCity(10, 4, "taipei"),
+        createCity(19, 8, "chongqing"),
+        createCity(16, 15, "beijing"),
+        createCity(5, 19, "shanghai"),
+        createCity(8, 10, "taiwan"),
     };
     printReport(cities);
     return 0;
